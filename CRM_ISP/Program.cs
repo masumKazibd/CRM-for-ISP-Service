819d843737c67dff754f27669ae586623d0ba72b
masumkazibd <masum.kazi.hantmk@gmail.com>
2023-12-24 18:46:38 +0600

    enable cors

--- CRM_ISP/Program.cs
+++ CRM_ISP/Program.cs
@@ -1,61 +1,3 @@
-//using CRM_ISP.Models;
-//using Microsoft.EntityFrameworkCore;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext<CrmDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("appCon")));
-//builder.Services.AddControllers()
-//     .AddNewtonsoftJson(option =>
-//     {
-//         option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
-//         option.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
-//     });
-
-//builder.Services.AddCors(options =>
-//{
-//    options.AddDefaultPolicy(
-//        builder =>
-//        {
-//            builder.WithOrigins("http://localhost:5285", "http://localhost:4200")
-//                                .AllowAnyHeader()
-//                                .AllowAnyOrigin()
-//                                .AllowAnyMethod();
-//        });
-//});
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseAuthorization();
-//app.UseCors(builder =>
-//{
-//    builder
-//    .AllowAnyOrigin()
-//    .AllowAnyMethod()
-//    .AllowAnyHeader();
-//});
-//app.MapControllers();
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller}/{action=Index}/{id?}");
-
-//app.Run();
-
-
-
-
 using CRM_ISP.AuthenticationPart.Services;
 using CRM_ISP.AuthenticationPart.Interfaces;
 using CRM_ISP.Models;
@@ -129,7 +71,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
